a0c87dedec2124d13efbe6c6e98cfad3f7a20cfd
segfault2k <sebastian.sariego.benitez@gmail.com>
2021-07-15 23:20:01 -0400

    apollo: overlay-corvus: Power button press to unlock with FP

--- device.mk
+++ device.mk
@@ -6,7 +6,8 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-corvus
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
