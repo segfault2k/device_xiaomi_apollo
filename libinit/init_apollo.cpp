0e9545e51b377cfb8226e2e0da9f5ac89f634290
Dobsgw <38656887+Dobsgw@users.noreply.github.com>
2021-07-29 06:48:12 +0100

    apollo: set redfin July fp for all variants

--- libinit/init_apollo.cpp
+++ libinit/init_apollo.cpp
@@ -14,8 +14,8 @@ static const variant_info_t apollocn_info = {
     .device = "apollo",
     .marketname = "K30S Ultra",
     .model = "M2007J3SC",
-    .build_description = "apollo-user 11 RKQ1.200826.002 V12.5.1.0.RJDCNXM release-keys",
-    .build_fingerprint = "Redmi/apollo/apollo:11/RKQ1.200826.002/V12.5.1.0.RJDCNXM:user/release-keys",
+    .build_description = "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+    .build_fingerprint = "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
 
     .nfc = true,
 };
@@ -28,8 +28,8 @@ static const variant_info_t apollo_info = {
     .device = "apollo",
     .marketname = "Mi 10T",
     .model = "M2007J3SY",
-    .build_description = "apollo-user 11 RKQ1.200826.002 V12.1.3.0.RJDMIXM release-keys",
-    .build_fingerprint = "Xiaomi/apollo_global/apollo:11/RKQ1.200826.002/V12.1.3.0.RJDMIXM:user/release-keys",
+    .build_description = "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+    .build_fingerprint = "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
 
     .nfc = true,
 };
@@ -42,8 +42,8 @@ static const variant_info_t apollopro_info = {
     .device = "apollo",
     .marketname = "Mi 10T Pro",
     .model = "M2007J3SG",
-    .build_description = "apollopro-user 11 RKQ1.200826.002 V12.1.3.0.RJDMIXM release-keys",
-    .build_fingerprint = "Xiaomi/apollopro_global/apollopro:11/RKQ1.200826.002/V12.1.3.0.RJDMIXM:user/release-keys",
+    .build_description = "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+    .build_fingerprint = "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
 
     .nfc = true,
 };
