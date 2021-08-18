41526f97af03b3c0542332ef095880e65837b971
segfault2k <sebastian.sariego.benitez@gmail.com>
2021-08-17 22:38:45 -0400

    apollo: Corvus bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollo.mk
+    $(LOCAL_DIR)/corvus_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollo-user \
-    lineage_apollo-userdebug \
-    lineage_apollo-eng
+    corvus_apollo-user \
+    corvus_apollo-userdebug \
+    corvus_apollo-eng
---
+++ corvus_apollo.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from apollo device
+$(call inherit-product, device/xiaomi/apollo/device.mk)
+
+# Inherit some common Corvus-OS stuff.
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
+
+#Official
+RAVEN_LAIR := Official
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.corvus.maintainer=segfault
+CORVUS_MAINTAINER := segfault
+
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+scr_resolution := 1080
+
+PRODUCT_NAME := corvus_apollo
+PRODUCT_DEVICE := apollo
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 10T
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
