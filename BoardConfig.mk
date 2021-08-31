LineageOS/android_device_xiaomi_apollon

apollon: libinit: Adapt to latest changes
Change-Id: I9a2cacdef922f19b788316537ef15394e5068edb

BoardConfig.mk
@@ -15,8 +15,8 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollon
-TARGET_RECOVERY_DEVICE_MODULES := libinit_apollon
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_apollon
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_apollon
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig

segfault2k/device_xiaomi_apollo

BoardConfig.mk
@@ -15,8 +15,8 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
-TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_apollo
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_apollo
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig