056375d3a43065e7780c960b865d9a2172adc6be
segfault2k <sebastian.sariego.benitez@gmail.com>
2021-09-13 17:27:31 -0300

    apollo: Sakura-ify!

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -12,14 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
 # Inherit some common HavocOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Havoc Bringup
-HAVOC_BUILD_TYPE := Official
-HAVOC_MAINTAINER := sewa2k
-HAVOC_GROUP_URL := https://t.me/HavocOS_apollo
-
+# Sakura Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+SAKURA_OFFICIAL := false
+SAKURA_MAINTAINER := sewa2k
 TARGET_GAPPS_ARCH := arm64
+SAKURA_BUILD_TYPE=basicgapps
+
 IS_PHONE := true
 
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -27,7 +28,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-PRODUCT_NAME := havoc_apollo
+PRODUCT_NAME := lineage_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
